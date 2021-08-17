bec8a128ae097e5062849512880fa2ba1d4ba027
Lee-Binny <qlsdl9516@gmail.com>
2021-08-17 20:58:15 +0900

    기본 환경 세팅

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+app.listen(port, () => {
+    console.log("server works on port: " + port);
+})
